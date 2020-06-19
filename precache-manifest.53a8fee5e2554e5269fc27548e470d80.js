b5c278dc4fd9e40bb2a3c5e3d7e1cd15f23a5c58
Caique <caiiquexd@gmail.com>
2020-06-19 18:38:52 -0300

    Updates

---
+++ precache-manifest.53a8fee5e2554e5269fc27548e470d80.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf8fa3b42b4f94e30ed23e909fa5936",
+    "url": "/TodoApp-React-Redux/index.html"
+  },
+  {
+    "revision": "b87080e17c5bf118b8de",
+    "url": "/TodoApp-React-Redux/static/css/2.becdaeb9.chunk.css"
+  },
+  {
+    "revision": "26b9183c143a4c14d693",
+    "url": "/TodoApp-React-Redux/static/css/main.09e95c8b.chunk.css"
+  },
+  {
+    "revision": "b87080e17c5bf118b8de",
+    "url": "/TodoApp-React-Redux/static/js/2.7c0b1607.chunk.js"
+  },
+  {
+    "revision": "6a4ddc26a393c242b00fc1a157b1908d",
+    "url": "/TodoApp-React-Redux/static/js/2.7c0b1607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b9183c143a4c14d693",
+    "url": "/TodoApp-React-Redux/static/js/main.6f181c3d.chunk.js"
+  },
+  {
+    "revision": "46c2fce74f9239d6f4a1",
+    "url": "/TodoApp-React-Redux/static/js/runtime-main.644487f4.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/TodoApp-React-Redux/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/TodoApp-React-Redux/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/TodoApp-React-Redux/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/TodoApp-React-Redux/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/TodoApp-React-Redux/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e05ec3d981d540adcba9cd4b4ab53f97.js"
+  "/TodoApp-React-Redux/precache-manifest.53a8fee5e2554e5269fc27548e470d80.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/TodoApp-React-Redux/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
